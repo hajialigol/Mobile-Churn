806d9ed5bde03eb978ff60d90f36bc06617c9d57
Daniel <danielhajialigol@gmail.com>
2020-07-11 12:45:21 -0400

    Fixed fitting issue

--- src/Mobile_Churn.py
+++ src/Mobile_Churn.py
@@ -1,11 +1,11 @@
 import seaborn as sns
 import pandas as pd
-import os
 import pyspark
 import pyspark.ml.evaluation as evals
 import pyspark.ml.tuning as tune
 from pyspark.sql import functions as F, SparkSession
-from pyspark.sql.functions import isnan, when, count, col, mean as _mean, stddev as _stddev
+from pyspark.sql.functions import isnan, when, count, col, lit, row_number, mean as _mean, stddev as _stddev
+from pyspark.sql.window import Window
 from pyspark.ml.feature import StringIndexer, OneHotEncoder, VectorAssembler, StandardScaler
 from pyspark.ml.classification import DecisionTreeClassifier, LogisticRegression
 from pyspark.ml import Pipeline
@@ -40,14 +40,53 @@ churn = drop_columns(churn, cols_to_drop)
 # ServiceArea
 churn = churn.filter(churn.ServiceArea.isNotNull())
 
-
 # HandsetPrice
 handset_mean = churn.select(_mean("HandsetPrice").alias("mean")).first()[0]
 churn = churn.withColumn("HandsetPrice", when(churn["HandsetPrice"] == "Unknown",
                                               handset_mean).otherwise(churn["HandsetPrice"]))
 
+def find_null_counts(pyspark_df):
+    '''
+    desc:
+        This function finds the columns that contain "NA"s represented as strings along with their length.
+    inpt:
+        pyspark_df [df]: Dataframe to find string "NA"s from.
+    oupt:
+        columns_string_nas [dict]: Dictionary of columns that have "NA"s represented as strings with the
+                                 key being the column name and the value being the amount of string "NA"s.
+    '''
+    columns_string_nas = {}
+    for column in pyspark_df.columns:
+        amount_string_nas = (pyspark_df.select(column).where(col(column) == "NA")).count()
+        if amount_string_nas > 0:
+            columns_string_nas[column] = amount_string_nas
+    return columns_string_nas
+
+
+def remove_nulls(pyspark_df, set_of_nulls):
+    '''
+    desc:
+        This function removes "NA"s of type string.
+    inpt:
+        pyspark_df [df]: PySpark dataframe to remove string nulls from.
+        set_of_nulls [set]: Set containing the columns that have "NA"s represented as strings.
+    oupt:
+        pyspark_df [df]: Updated PySpark dataframe without string "NA"s.
+    '''
+    for column in set_of_nulls:
+        pyspark_df = pyspark_df.filter(col(column) != "NA")
+    return pyspark_df
+
+null_dict = find_null_counts(churn)
+null_set = set(null_dict.keys())
+churn = remove_nulls(churn, null_set)
+
 
 # (3) Convert columns to correct data type
+string_columns = {"Churn", "ServiceArea", "ChildrenInHH", "HandsetWebCapable", "RespondsToMailOffers",
+                  "OptOutMailings", "NonUSTravel", "OwnsComputer", "HasCreditCard", "NewCellphoneUser",
+                  "MadeCallToRetentionTeam", "CreditRating", "PrizmCode", "Occupation", "MaritalStatus"}
+
 def casting(df, string_cols):   
     '''
     desc:
@@ -63,13 +102,9 @@ def casting(df, string_cols):
             df = df.withColumn(column, df[column].cast("double"))
     return df
 
-string_columns = {"Churn", "ServiceArea", "ChildrenInHH", "HandsetWebCapable", "RespondsToMailOffers",
-                  "OptOutMailings", "NonUSTravel", "OwnsComputer", "HasCreditCard", "NewCellphoneUser",
-                  "MadeCallToRetentionTeam", "CreditRating", "PrizmCode", "Occupation", "MaritalStatus"}
 churn = casting(churn, string_columns)
 
 
-# Convert response variable into integer column (default response variable name in PySpark is "label") 
 churn = churn.withColumn("label", col("Churn") == "Yes")
 churn = churn.withColumn("label", col("label").cast('int'))
 
@@ -109,11 +144,14 @@ def ml_pipeline(pyspark_df):
 
 piped = ml_pipeline(churn)
 
+
 # (6) Standardize dataset
 training, testing = piped.randomSplit([.75, .25])
 
+
 # (7) Create evaluator
 evaluator = evals.BinaryClassificationEvaluator(metricName = "areaUnderROC")
+
 
 # (8) Create model
 lr = LogisticRegression()
